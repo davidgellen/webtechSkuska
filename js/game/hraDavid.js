495832183250282955fcf2424762e7268c562050
davidgellen <gellen.david1@gmail.com>
2020-12-26 15:42:50 +0100

    hratelne bez dema ja na telefone

--- js/game/hraDavid.js
+++ js/game/hraDavid.js
@@ -1,71 +1,68 @@
+// robene kniznicou pixi.js
+// nazorny priklad: http://scottmcdonnell.github.io/pixi-examples/index.html?s=demos&f=dragging.js&title=Dragging 
+
 const kraje = [
     {
         name: "BA",
         src: "../res/imagesHraDavid/baKraj.png",
-        drop: {x1: 340, x2: 410, y1: 160, y2: 220, x: 387, y: 190}
+        drop: {x1: 0, x2: 67, y1: 160, y2: 220, x: 44, y: 190}
     },
     {
         name: "TT",
         src:  "../res/imagesHraDavid/ttKraj.png",
-        drop: {x1: 390, x2: 440, y1: 128, y2: 257, x: 412, y: 191}
+        drop: {x1: 47, x2: 97, y1: 128, y2: 257, x: 69, y: 191}
     },
     {
         name: "TR",
         src:  "../res/imagesHraDavid/trKraj.png",
-        drop: {x1: 414, x2: 513, y1: 80, y2: 146, x: 471, y: 111}
+        drop: {x1: 71, x2: 170, y1: 80, y2: 146, x: 128, y: 111}
     },
     {
         name: "NR",
         src:  "../res/imagesHraDavid/nrKraj.png",
-        drop: {x1: 449, x2: 520, y1: 175, y2: 260, x: 497, y: 208}
+        drop: {x1: 106, x2: 177, y1: 175, y2: 260, x: 153, y: 208}
     },
     {
         name: "ZI",
         src:  "../res/imagesHraDavid/ziKraj.png",
-        drop: {x1: 514, x2: 626, y1: 30, y2: 119, x: 574, y: 75}
+        drop: {x1: 171, x2: 283, y1: 30, y2: 119, x: 231, y: 75}
     },
     {
         name: "BB",
         src:  "../res/imagesHraDavid/bbKraj.png",
-        drop: {x1: 515, x2: 656, y1: 128, y2: 227, x: 597, y: 170}
+        drop: {x1: 172, x2: 313, y1: 128, y2: 227, x: 254, y: 170}
     },
     {
         name: "PR",
         src:  "../res/imagesHraDavid/prKraj.png",
-        drop: {x1: 660, x2: 841, y1: 51, y2: 100, x: 754, y: 84}
+        drop: {x1: 317, x2: 498, y1: 51, y2: 100, x: 411, y: 84}
     },
     {
         name: "KE",
         src:  "../res/imagesHraDavid/keKraj.png",
-        drop: {x1: 678, x2: 834, y1: 122, y2: 171, x: 762, y: 146}
+        drop: {x1: 335, x2: 491, y1: 122, y2: 171, x: 419, y: 146}
     },
 ]
 
 var matched = 0;
 
-var clientWidth = document.documentElement.clientWidth;
-var clientHeight = document.documentElement.clientHeight;
-
-var renderer = PIXI.autoDetectRenderer(Math.max(document.documentElement.clientWidth*0.9 || 0, window.innerWidth*0.9 || 0), 300);
+var renderer = PIXI.autoDetectRenderer(550, 300);
 document.getElementById("game").appendChild(renderer.view);
 var stage = new PIXI.Container();
-
-var imageScale = 1;
-var widthRatio;
 
 function createKraje() {
     let randomKrajeArray = shuffleArray(kraje);
     for (let i = 0; i < randomKrajeArray.length; i++){
         let kraj = PIXI.Texture.from(randomKrajeArray[i].src);
         //kraje.baseTexture.scaleMode = PIXI.SCALE_MODES.NEAREST;
-        createKraj(Math.floor(Math.random() * clientWidth*0.9) , 250, kraj, randomKrajeArray[i].drop);
+        createKraj(Math.floor(Math.random() * 500) , Math.floor(Math.random() * 250), kraj, randomKrajeArray[i].drop);
     }
 }
 
 function createBackground(){
     let countryTexture = PIXI.Texture.from("../res/imagesHraDavid/SKblank.png");
     let country = new PIXI.Sprite(countryTexture);
-    country.position.x = (clientWidth*0.9)/2 - 534/2;
+    country.position.x = 5;
     country.position.y = 11;
     stage.addChild(country);
 }
@@ -94,7 +91,7 @@ function createKraj(x, y, texture, dropArea)
     stage.addChild(kraj);
 }
 
-requestAnimationFrame( animate );
+requestAnimationFrame(animate);
 
 function animate() {
     requestAnimationFrame(animate);
@@ -127,8 +124,7 @@ function onDragEnd()
 
 function onDragMove()
 {
-    if (this.dragging)
-    {
+    if (this.dragging) {
         let newPosition = this.data.getLocalPosition(this.parent);
         this.position.x = newPosition.x;
         this.position.y = newPosition.y;
@@ -166,10 +162,17 @@ function gameOver(){
 
 function showGameOverText(){
     let text = new PIXI.Text('Hra dokončená',{fontFamily : 'Arial', fontSize: 50, fill : 0xff1010, });
-    text.position.x = (clientWidth*0.9)/2 - 130;
-    text.position.y = 125;
+    text.position.x = 100;
+    text.position.y = 100;
     stage.addChild(text);
 }
+
+
+// stopky
+// https://tinloof.com/blog/how-to-build-a-stopwatch-with-html-css-js-react-part-2/
+
+var startTime;
+var timerInterval;
 
 function timeToString(time) { // matika s casom
     let hours = time / 3600000;
@@ -180,7 +183,7 @@ function timeToString(time) { // matika s casom
     let roundSeconds = Math.floor(seconds);
     let minutesString = roundMinutes.toString().padStart(2, "0");
     let secondsString = roundSeconds.toString().padStart(2, "0");
-    return minutesString+" : "+secondsString;
+    return minutesString + " : " + secondsString;
 }
 
 function printTime() {
@@ -188,7 +191,6 @@ function printTime() {
     document.getElementById("gameTime").innerHTML = timeToString(elapsedTime);
 }
 
-var timerInterval;
 function startClock() {
     startTime = Date.now();
     timerInterval = setInterval(printTime, 1000);
@@ -220,6 +222,7 @@ function startGame(){
     startClock();
 }
 
-function getTrueWidthRatio(origX){ // matika proste
-    return origX*100/1229.4;
-}
+/*function getTrueWidthRatio(origX){ // prepocet kvoli responzivite
+    let ratio = origX*100/1229.4;
+    return clientWidth*0.9/100*ratio; // marna snaha bude to natvrdo
+}*/
